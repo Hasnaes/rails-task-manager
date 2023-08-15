3f11cf1aa2b143f35731020af64333c4216771b9
HasnaeSn <hasnae.sdn@outlook.com>
2023-08-15 21:58:14 +0200

    almost finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :tasks
   # Defines the root path route ("/")
   # root "articles#index"
+  # Read all tasks
+  # get '/tasks', to: 'tasks#index'
+
+  # # Create new task
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
+
+  # # Update task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :update_task
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # Delete task
+  # delete 'tasks/:id',  to: 'tasks#destroy'
+
+  # # Read one task
+  # get '/tasks/:id', to: 'tasks#show', as: :task
 end
